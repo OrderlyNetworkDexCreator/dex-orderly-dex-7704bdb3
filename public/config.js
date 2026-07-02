cd0f7f314e0fee97dc3c6a31de1883726e564cbb
orderly-guoqing <guoqing@orderly.network>
2026-07-02 19:27:39 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/dex-orderly-dex-7704bdb3/",
+  "VITE_SEO_SITE_URL": "https://metadata.google.internal.example.com",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
